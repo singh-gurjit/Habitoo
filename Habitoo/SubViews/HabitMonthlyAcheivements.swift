0fed5e724cd8996c29f6cae887fe1ad6eec01fff
Gurjit Singh <gurjit.singh.engineer@gmail.com>
2020-06-29 11:29:09 +0100

    Added functionality to search differences and count difference between elements in array

--- Habitoo/SubViews/HabitMonthlyAcheivements.swift
+++ Habitoo/SubViews/HabitMonthlyAcheivements.swift
@@ -32,14 +32,24 @@ struct HabitMonthlyAcheivements: View {
         }.onAppear(){
             self.fetchResultFromDatabase = self.databaseUtil.habitRecordForThisMonth(hID: self.habitID) as! [Date]
             self.fetchResultFromDatabaseFiltered = self.dateUtil.filterDateFromCurrentMonth(array: self.fetchResultFromDatabase)
+            
             self.convertToInt = self.fetchResultFromDatabaseFiltered.map { Int($0)!}
-            //let consecutives = self.convertToInt.map { $0 - 1 }.dropFirst() == self.convertToInt.dropLast()
-            let output = stride(from: 0, to: self.convertToInt.count - 1, by: 2).map{(self.convertToInt[$0], self.convertToInt[$0 + 1])}
+            
+            let input = [1,2,4,6,7,10,12,13,14,18,19,33]
+            //check two elements and compare them and find element consecutive
+            let output = stride(from: 0, to: input.count - 1, by: 2).map{(input[$0], input[$0 + 1])}
+            //differences between elements
             let differences = output.map({ $0.1 - $0.0 })
+            //how many are consitence
             let onesCount = differences.filter({ $0 == 1}).count
+            let twoCount = differences.filter({ $0 == 2}).count
+            let threeCount = differences.filter({ $0 == 7}).count
 
             print(differences)
             print(onesCount)
+            print(twoCount)
+            print(threeCount)
+            
             print("filtered data - \(self.fetchResultFromDatabaseFiltered)")
         }
     }
